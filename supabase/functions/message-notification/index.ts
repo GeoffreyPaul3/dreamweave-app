e2c4a7b3e27a525caa6e528bd52c907bf9e3c9b8
GeoffreyPaul3 <geofreypaul40@gmail.com>
2025-07-09 14:46:00 +0200

    Fixed Emails

--- supabase/functions/message-notification/index.ts
+++ supabase/functions/message-notification/index.ts
@@ -3,7 +3,7 @@
 // Email notification edge function for new messages
 
 // @ts-expect-error Deno global is provided by the runtime
-const RESEND_API_KEY = Deno.env.get('VITE_RESEND_API_KEY');
+const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY');
 const FROM_EMAIL = 'DreamWeave <notifications@dreamweave.mw>';
 
 async function sendEmail({ to, subject, html }: { to: string; subject: string; html: string }) {
